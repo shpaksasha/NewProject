0768ce6073d746b9def37a57eb884bb324aee969
shpaksasha <shpaksasha0000@gmail.com>
2021-12-04 22:42:24 +0200

    add folder and file home & home_page.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores debitis dicta, distinctio
+            doloremque eveniet expedita fugit laboriosam maxime neque odio provident quo, saepe sint soluta tempora
+            velit. Ab blanditiis ea facilis id labore molestiae reprehenderit sapiente tempore! A aut beatae dolor,
+            ducimus fugiat libero numquam odit quibusdam sint ut!
         </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-);
+)
 
 export default App;
---
+++ src/Home/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias aperiam atque blanditiis, cupiditate
+            deleniti deserunt dolorem dolores eos esse facilis fugiat harum hic illum ipsum, itaque iure labore
+            laboriosam minus modi mollitia nihil nisi numquam odit officia officiis omnis optio pariatur porro quas
+            ratione recusandae repellendus, ut veniam voluptatem voluptates! Amet animi cum dicta in itaque laudantium
+            modi mollitia nostrum obcaecati officiis pariatur, quis tempore totam vero vitae. Consequuntur dignissimos
+            dolore dolorem error et in incidunt necessitatibus neque nobis odio, officiis pariatur, perferendis quis
+            rerum sed similique sint tempora ut vel, veritatis! Ab at harum omnis porro tempora!
+        </div>
+    );
+};
+
+export default Home;
