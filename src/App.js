7264a9d4b345176bc91bd966f1c0bc5d4cfc0402
shpaksasha <shpaksasha0000@gmail.com>
2021-12-19 16:33:08 +0200

    page header material ui styles add

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from "./pages/Header/header";
 import Home from './pages/Home/home';
 import About from "./pages/About/about";
 import Faq from "./pages/Faq/faq";
+import Container from '@mui/material/Container';
 
 const App = () => {
     return (
+
         <Fragment>
             <Header/>
+            <Container maxWidth="lg" sx={{backgroundColor: '#f5f5f5', height: '100vh'}}>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/about' element={<About/>}/>
                 <Route path='/faq' element={<Faq/>}/>
             </Routes>
+            </Container>
         </Fragment>
+
     )
 }
 
--- src/pages/Header/header.js
+++ src/pages/Header/header.js
@@ -1,12 +1,52 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import {makeStyles} from "@mui/styles";
+import {AppBar, Toolbar, Typography, TextField} from "@mui/material";
+// import TextField from "@mui/material/TextField";
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        margin: '0rem',
+        display: 'flex',
+        alignItems: 'center',
+
+    },
+    link: {
+        padding: '0px 40px',
+        textAlign: 'center',
+        position: 'relative',
+        textDecoration: 'none',
+        color: '#ffeb3b',
+        fontSize: '25px',
+        // lineHeight: '20px',
+        fontFamily: 'Roboto, sans-serif',
+        // [theme.breakpoints.down('sm')]: {
+        //     fontSize: '12px',
+        //     lineHeight: '18px',
+        // },
+        // [theme.breakpoints.down('xs')]: {
+        //     justifyContent: 'space-between',
+        //     fontSize: '25px',
+        //     lineHeight: '45px',
+        // }
+    },
+}));
+
 
 const Header = () => {
+    const classes = useStyles();
     return (
         <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/faq'>FAQ</NavLink>
+        <AppBar position="static" className={classes.root}>
+            <Toolbar>
+                <Typography component="div"><NavLink className={classes.link} to='/'>Home</NavLink></Typography>
+                <Typography component="div"><NavLink className={classes.link} to='/about'>About</NavLink></Typography>
+                <Typography component="div"><NavLink className={classes.link} to='/faq'>FAQ</NavLink></Typography>
+            </Toolbar>
+        </AppBar>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         </div>
     )
 }
