c3fc3c507fa86188c888da39e94a8e3492b9ed52
shpaksasha <shpaksasha0000@gmail.com>
2021-12-27 15:48:19 +0200

    add post.styles.js add classes at new file

--- src/components/Post/post.js
+++ src/components/Post/post.js
@@ -1,29 +1,6 @@
 import React from 'react';
-import {makeStyles} from "@mui/styles";
+import {useStyles} from "./post.styles"
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        position: 'relative',
-        display: 'block',
-        marginTop: '20px',
-        border: '2px solid black',
-        alignItems: 'center'
-
-    },
-    description: {
-
-        display: 'block',
-        justifyContent: 'space-between',
-        padding: '10px',
-        alignItems: 'center',
-    },
-    button: {
-        display: 'block',
-        position: 'absolute',
-        top: '50%',
-        right: 0,
-    }
-}))
 
 const Post = () => {
     const classes = useStyles();
---
+++ src/components/Post/post.styles.js
@@ -0,0 +1,27 @@
+import {makeStyles} from "@mui/styles";
+
+export const useStyles = makeStyles(theme => ({
+
+root: {
+    position: 'relative',
+    display: 'block',
+    marginTop: '20px',
+    border: '2px solid black',
+    alignItems: 'center'
+
+},
+
+description: {
+
+    display: 'block',
+    justifyContent: 'space-between',
+    padding: '10px',
+    alignItems: 'center',
+},
+button: {
+    display: 'block',
+    position: 'absolute',
+    top: '50%',
+    right: 0,
+}
+}))
