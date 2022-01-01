ab72c17c2f4368aae3b10751458e1ac5fb9b7262
shpaksasha <shpaksasha0000@gmail.com>
2022-01-01 21:41:14 +0200

    add form - two field and button, change styles

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,7 +1,30 @@
 import React, {useState} from 'react';
 import {Post} from "../../components/Post/post";
+import {Button} from "@mui/material";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import {makeStyles} from "@mui/styles";
+
+const useStyles = makeStyles(theme => ({
+    box: {
+        margin: 30,
+        justifyContent: 'space-around',
+        display: 'flex',
+        padding: '0px 10px'
+    },
+    field: {
+        width: 400,
+    },
+    button: {
+        maxHeight: 37,
+        fontFamily: 'Roboto Condensed',
+        fontStyle: 'italic',
+    }
+}))
 
 const Home = () => {
+
+    const classes = useStyles();
 
     const [posts, setPosts] = useState([
         {id: 1, title: 'Java Script', body: 'Hello Aleks'},
@@ -11,18 +34,34 @@ const Home = () => {
 
     return (
         <div>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias aperiam atque blanditiis,
-            cupiditate
-            deleniti deserunt dolorem dolores eos esse facilis fugiat harum hic illum ipsum, itaque iure labore
-            laboriosam minus modi mollitia nihil nisi numquam odit officia officiis omnis optio pariatur porro quas
-            ratione recusandae repellendus, ut veniam voluptatem voluptates! Amet animi cum dicta in itaque
-            laudantium
-            modi mollitia nostrum obcaecati officiis pariatur, quis tempore totam vero vitae. Consequuntur
-            dignissimos
-            dolore dolorem error et in incidunt necessitatibus neque nobis odio, officiis pariatur, perferendis quis
-            rerum sed similique sint tempora ut vel, veritatis! Ab at harum omnis porro tempora!
-
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias aperiam atque blanditiis,
+                cupiditate
+                deleniti deserunt dolorem dolores eos esse facilis fugiat harum hic illum ipsum, itaque iure labore
+                laboriosam minus modi mollitia nihil nisi numquam odit officia officiis omnis optio pariatur porro quas
+                ratione recusandae repellendus, ut veniam voluptatem voluptates! Amet animi cum dicta in itaque
+                laudantium
+            </p>
+            <form>
+                <Box className={classes.box}>
+                    <TextField
+                        helperText='Введите описание'
+                        id='demo'
+                        label='Описание'
+                        className={classes.field}
+                        size='small'
+                    />
+                    <TextField
+                        helperText='Введите название'
+                        id='demo-helper'
+                        label='Название'
+                        className={classes.field}
+                        size='small'
+                    />
+                    <Button className={classes.button} size='small' variant='contained' color='success'>Создать пост</Button>
+                </Box>
+            </form>
             <div>
+
                 <h2 style={{textAlign: 'center'}}>Список постов</h2>
                 {posts.map(item =>
                     <Post post={item} key={item.id}/>
