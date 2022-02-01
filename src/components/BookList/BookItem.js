a3a36e0ca4829728d407cf2ca434c956ff9c689f
shpaksasha <shpaksasha0000@gmail.com>
2022-02-01 23:12:23 +0200

    component BookItem changes, add new styles as matereal.ui

--- src/components/BookList/BookItem.js
+++ src/components/BookList/BookItem.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import {Grid} from "@mui/material";
+import {Button, Card, CardActions, CardContent, CardMedia, Grid, Typography} from '@mui/material';
 
 const BookItem = (props) => {
-    const {name, price, setOrder} = props;
+    const {name, price, poster, setOrder} = props;
 
     return (
         <Grid item sm={12} md={4}>
-            <img
-                src={`https://via.placeholder.com/300x150.png?text=${name.slice(0, 10)}`}
-                alt={name}/>
-            <div>
-                <h4>{name}</h4>
-                <p>Цена: {price} грн.</p>
-                <button onClick={() =>
-                    setOrder({
-                        id: props.id,
-                        name: props.name,
-                        price: props.price,
-                    })
-                }>Купить
-                </button>
-            </div>
+            <Card sx={{minHeight: 342}}>
+                <CardMedia
+                    image={poster}
+                    title={name}
+                    alt={name}
+                    sx={{height: 175}}
+                />
+                <CardContent>
+                    <Typography variant='h6' component='div'>{name}</Typography>
+                    <Typography variant='body1'>Цена: {price} грн.</Typography>
+                </CardContent>
+
+                <CardActions>
+                    <Button variant='contained'
+                            size='small'
+                            onClick={() =>
+                                setOrder({
+                                    id: props.id,
+                                    name: props.name,
+                                    price: props.price,
+                                })
+                            }>Купить
+                    </Button>
+                </CardActions>
+            </Card>
         </Grid>
     );
 };
