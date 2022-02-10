c3a980be0f9be0c2f183545a3fbc570a4416dedc
shpaksasha <shpaksasha0000@gmail.com>
2022-02-10 20:19:48 +0200

    add changes to form

--- src/pages/Form/Form.js
+++ src/pages/Form/Form.js
@@ -2,9 +2,56 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 
 const Form = () => {
+    const {
+        register,
+        formState: {errors},
+        handleSubmit,
+        reset
+    } = useForm({mode: 'onBlur'})
+
+    const submit = (data) => {
+        alert(JSON.stringify(data))
+        reset()
+    }
+
     return (
         <div>
-            
+            <h1>React-Hook-Form</h1>
+            <form onSubmit={handleSubmit(submit)}>
+                <div>
+                <label>FirstName
+                    <input
+                        {...register('firstName', {
+                            required: 'Поле обязательно для заполнения',
+                            minLength: {
+                                value: 10,
+                                message: 'min количество символов 10 '
+                            }
+                        })}
+                    />
+                </label>
+                <div style={{height: 10}}>
+                    {errors?.firstName && <p>{errors?.firstName?.message || 'Error!'}</p>}
+                </div>
+                </div>
+                <div>
+                <label>LastName
+                    <input
+                        {...register('lastName', {
+                            required: 'Поле обязательно для заполнения',
+                            minLength: {
+                                value: 10,
+                                message: 'min количество символов 10 '
+                            }
+                        })}
+                    />
+                </label>
+                <div style={{height: 10}}>
+                    {errors?.lastName && <p>{errors?.lastName?.message || 'Error!'}</p>}
+                </div>
+                </div>
+                <input type='submit'/>
+            </form>
         </div>
     );
 };
