8bd684c63c9f515ce2cbf5fe5337a19c15266f62
shpaksasha <shpaksasha0000@gmail.com>
2022-02-13 22:46:02 +0200

    install firebase, and firebase init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgTbRasYiXHTs9ZSNeBvkz6uwA7KOIKZI",
+    authDomain: "new-project-b4d6d.firebaseapp.com",
+    projectId: "new-project-b4d6d",
+    storageBucket: "new-project-b4d6d.appspot.com",
+    messagingSenderId: "209989473981",
+    appId: "1:209989473981:web:d78e9d7a8238f8c5ca09d4"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
     <BrowserRouter>
